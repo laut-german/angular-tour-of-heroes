2f4d1d5627b9fc479529c0407f23f3dd5d093a13
Lautaro Germán Zuñiga Garrido <lautaro.zuniga@stevensolutions.es>
2018-06-25 13:36:32 +0200

    Inyeccion de servicio

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes : Hero[];
   selectedHero: Hero;
 
-
-  constructor() { }
+ constructor(private heroService: HeroService) { }
 
   ngOnInit() {
- 
+    this.getHeroes(); //Primero se llama aquí
   }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    console.log("selectedHero: " + hero.name);
+    //console.log("selectedHero: " + hero.name);
+  }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes(); //Segundo se llama aquí, rellenando el array que luego se pintara en pantalla
   }
 }
