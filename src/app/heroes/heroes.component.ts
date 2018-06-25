b9ac2529b9fd7da8900a5e4a3ea7b17b2bf10462
Lautaro Germán Zuñiga Garrido <lautaro.zuniga@stevensolutions.es>
2018-06-25 12:48:08 +0200

    Lista con seleccion

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,10 +8,16 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
+  selectedHero: Hero;
+
+
   constructor() { }
 
   ngOnInit() {
  
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log("selectedHero: " + hero.name);
+  }
 }
